645bc0bd6e17a9e214249bf92b7cf949b5d8213f
JJpower80 <jcp0049@alu.medac.es>
2026-04-13 18:24:17 +0200

    Integracion de IA

--- pruebaIA.py
+++ pruebaIA.py
@@ -1,9 +1,13 @@
 from google import genai
+from google.genai import types
 
 client = genai.Client()
 
 response = client.models.generate_content(
     model="gemini-3-flash-preview",
-    contents="How does AI work?"
+    contents="How does AI work?",
+    config=types.GenerateContentConfig(
+        thinking_config=types.ThinkingConfig(thinking_level="low")
+    ),
 )
 print(response.text)
