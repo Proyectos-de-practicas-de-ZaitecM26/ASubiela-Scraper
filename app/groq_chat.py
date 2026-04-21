a7c6c244ae09ba9fa5c56f0515c9b5400332a44a
amm0246-a11y <amm0246@alu.medac.es>
2026-04-21 10:59:48 +0200

    Cambios realizados en el proyecto:

--- app/groq_chat.py
+++ app/groq_chat.py
@@ -1,19 +1,26 @@
 import os
-
 from groq import Groq
+from dotenv import load_dotenv
 
-client = Groq(
-    api_key=os.environ.get("GROK_KEY"),
-)
+# Cargamos las variables del archivo .env
+load_dotenv()
 
-chat_completion = client.chat.completions.create(
-    messages=[
-        {
-            "role": "user",
-            "content": "Explain the importance of fast language models",
-        }
-    ],
-    model="llama-3.3-70b-versatile",
-)
+def obtener_respuesta_groq(mensaje_usuario):
+    try:
+        client = Groq(
+            api_key=os.environ.get("GROQ_KEY"),
+        )
 
-print(chat_completion.choices[0].message.content)
+        chat_completion = client.chat.completions.create(
+            messages=[
+                {
+                    "role": "user",
+                    "content": mensaje_usuario,
+                }
+            ],
+            model="llama-3.3-70b-versatile",
+        )
+
+        return chat_completion.choices[0].message.content
+    except Exception as e:
+        return f"Error al conectar con Groq: {str(e)}"
